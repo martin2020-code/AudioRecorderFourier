1cea44f1c5ff3f0e005c58ca9b7bf691ea4f012c
martin2020-code <82909403+martin2020-code@users.noreply.github.com>
2023-01-22 14:57:23 -0500

    bottom sheet behavior

--- app/src/main/java/com/example/audiorecorderfourier/MainActivity.kt
+++ app/src/main/java/com/example/audiorecorderfourier/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.audiorecorderfourier
 import android.content.Context
 import android.content.pm.PackageManager
 import android.media.MediaRecorder
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.VibrationEffect
-import android.os.Vibrator
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.bottom_sheet.*
+import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
@@ -17,6 +22,7 @@ const val REQUEST_CODE = 200
 
 class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
 
+    private lateinit var amplitudes: ArrayList<Float>
     private var permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO)
     private var permissionGranted = false
 
@@ -32,6 +38,8 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
 
     private lateinit var timer: Timer
 
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -39,6 +47,10 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
 
         if (!permissionGranted)
             ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.peekHeight = 0
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
 
         timer = Timer(this)
         vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
@@ -52,6 +64,61 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
 
             vibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE))
         }
+
+        btnDone.setOnClickListener{
+            stopRecorder()
+            Toast.makeText(this, "Record saved", Toast.LENGTH_SHORT).show()
+
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            bottomSheetBG.visibility = View.VISIBLE
+            filenameInput.setText(filename)
+        }
+
+        btnCancel.setOnClickListener {
+            File("$dirPath$filename.mp3").delete()
+            dismiss()
+        }
+
+        btnOK.setOnClickListener{
+            dismiss()
+            save()
+        }
+
+        bottomSheetBG.setOnClickListener{
+            File("$dirPath$filename.mp3").delete()
+            dismiss()
+        }
+
+        btnDelete.setOnClickListener{
+            stopRecorder()
+            File("$dirPath$filename.mp3").delete()
+            Toast.makeText(this, "Record deleted", Toast.LENGTH_SHORT).show()
+        }
+
+        btnDelete.isClickable = false
+    }
+
+    private fun save(){
+        val newFilename = filenameInput.text.toString()
+        if (newFilename != filename){
+            var newFile = File("$dirPath$newFilename.mp3")
+            File("$dirPath$filename.mp3").renameTo(newFile)
+        }
+    }
+
+    private fun dismiss(){
+        bottomSheetBG.visibility = View.GONE
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        hideKeyBoard(filenameInput)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }, 100)
+    }
+
+    private fun hideKeyBoard(view: View){
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     override fun onRequestPermissionsResult(
@@ -112,10 +179,34 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
         isPaused = false
 
         timer.start()
+
+        btnDelete.isClickable = true
+        btnDelete.setImageResource(R.drawable.ic_delete)
+
+        btnList.visibility = View.GONE
+        btnDone.visibility = View.VISIBLE
     }
 
     private fun stopRecorder(){
         timer.stop()
+
+        recorder.apply {
+            stop()
+            release()
+        }
+
+        isPaused = false
+        isRecording = false
+
+        btnList.visibility = View.VISIBLE
+        btnDone.visibility = View.GONE
+
+        btnDelete.isClickable = false
+        btnDelete.setImageResource(R.drawable.ic_delete_disabled)
+        btnRecord.setImageResource(R.drawable.ic_record)
+
+        tvTimer.text = "00:00.00"
+        amplitudes = waveformView.clear()
     }
 
     override fun onTimerTick(duration: String) {
